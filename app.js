40315cfeee315247f564e95cf602d9376a60159a
Shreyyyy <shreyk1999@gmail.com>
2020-07-19 02:28:51 +0530

    added express

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+// express app
+const app = express();
+ 
+app.get('/', (req,res) => {
+    res.sendFile('./views/index.html',{root : __dirname})
+});
+
+app.get('/about', (req,res) => {
+    res.sendFile('./views/about.html',{root : __dirname})
+});
+
+//redirects
+app.get('/about-us', (req,res) => {
+    res.redirect('/about')
+})
+
+app.use((req,res) => {
+    res.sendFile('./views/404.html',{root : __dirname})
+});
+
+//listen for requests
+app.listen(3000)
