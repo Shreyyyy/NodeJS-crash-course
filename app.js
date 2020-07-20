2f8747ebf8f1b22bd5a503aae12461f3dace88cd
Shreyyyy <shreyk1999@gmail.com>
2020-07-20 16:22:02 +0530

    Added partials

--- app.js
+++ app.js
@@ -8,20 +8,30 @@ const app = express();
 app.set('view engine','ejs');
 
 app.get('/', (req,res) => {
-    res.render('index')
+    const blogs = [
+    {title: 'Yoshi finds eggs', description: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', description: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', description: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+    res.render('index',{title: 'Home' , blogs});
 });
 
 app.get('/about', (req,res) => {
-    res.render('about')
+    res.render('about',{title: 'About'});
 });
+
+app.get('/blogs/create',(req,res) => {
+    res.render('create',{title: 'Create Blog'});
+})
 
 //redirects
 app.get('/about-us', (req,res) => {
     res.redirect('/about')
 })
 
+
 app.use((req,res) => {
-    res.status(404).render('404');
+    res.status(404).render('404',{title:'404'});
 });
 
 //listen for requests
